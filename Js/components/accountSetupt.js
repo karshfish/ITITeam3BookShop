e43861812155aef5be094873390d0d014529030d
karshfish <karshfish35@gmail.com>
2025-08-19 14:38:56 +0300

    kdhgd

--- Js/components/accountSetupt.js
+++ Js/components/accountSetupt.js
@@ -1,6 +1,8 @@
 export function setupAccountIcon() {
   const iconHTML = document.getElementById("user-account");
   let current = localStorage.getItem("currentUser");
+  let menu = document.getElementById("dropdown-menu");
+  console.log(menu);
   if (!current) {
     iconHTML.innerHTML = "Sign up or Login";
     let parent = iconHTML.parentNode;
@@ -12,7 +14,18 @@ export function setupAccountIcon() {
     iconHTML.innerHTML = `Welcom ${name} `;
     let parent = iconHTML.parentNode;
 
+    let logout = document.createElement("a");
+    logout.className = `dropdown-item`;
+    logout.innerHTML = "Logout";
+    logout.addEventListener("click", function () {
+      localStorage.removeItem("cart");
+      localStorage.removeItem("currentUser");
+      window.location.href = "home.html";
+    });
+
+    menu.appendChild(logout);
     parent.setAttribute("href", "#");
+    parent.appendChild(dropDown);
     console.log(parent);
   }
 }
