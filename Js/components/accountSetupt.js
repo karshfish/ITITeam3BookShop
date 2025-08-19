78af7c12529caf303fc4c0cd99f060f5fb112736
karshfish <karshfish35@gmail.com>
2025-08-19 13:17:14 +0300

    Update accountSetupt.js

--- Js/components/accountSetupt.js
+++ Js/components/accountSetupt.js
@@ -3,6 +3,9 @@ export function setupAccountIcon() {
   let current = localStorage.getItem("currentUser");
   if (!current) {
     iconHTML.innerHTML = "Sign up or Login";
+    let parent = iconHTML.parentNode;
+
+    parent.setAttribute("href", "log_reg.html");
   } else {
     let name = JSON.parse(current);
     name = name.fullName;
