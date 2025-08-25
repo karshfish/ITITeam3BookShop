fb3f6bf6b8e4bda524d59638b60b8fa790fa5c3a
karshfish <karshfish35@gmail.com>
2025-08-25 16:30:50 +0300

    loadingbooks

--- Js/startup/loadBooks.js
+++ Js/startup/loadBooks.js
@@ -1,15 +1,13 @@
-async function loadBooks() {
+export async function loadBooks() {
+  const response = await fetch("./booksv3.0.json");
+  const data = await response.json();
 
-    const response = await fetch("../booksv3.0.json");
-    const data = await response.json();
+  // Only keep first 50
+  const limitedBooks = data.slice(60, 120);
 
-    // Only keep first 50
-    const limitedBooks = data.slice(60, 120);
-
-    localStorage.setItem("products", JSON.stringify(limitedBooks));
-    // console.log("50 Books saved to localStorage:", limitedBooks);
-
-  } 
+  return limitedBooks;
+  // console.log("50 Books saved to localStorage:", limitedBooks);
+}
 
 // Run
-loadBooks();
+export const products = await loadBooks();
